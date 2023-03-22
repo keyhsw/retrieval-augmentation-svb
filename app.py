05feb2be2b1fbd957faa7b6b5f9e151013fef40a
oryx1729 <78848855+oryx1729@users.noreply.github.com>
2023-03-22 10:50:33 +0100

    Add pipeline

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-import sys
 import logging
+import sys
+
 import streamlit as st
-import haystack
+from haystack import Document
+from haystack import Pipeline
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.nodes import EmbeddingRetriever
+from haystack.nodes import FARMReader
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -11,6 +16,29 @@ logging.basicConfig(
 )
 
 
-st.title("Haystack Demo")
-st.text_input("Query ...")
-st.text(haystack.__version__)
+def app_init():
+    docs = [Document(id='1', content='His name is John.'),
+            Document(id='2', content='Her name is Jane.'),
+            Document(id='3', content='My name is Haystack.')]
+    ds = InMemoryDocumentStore()
+    ds.write_documents(docs)
+    retriever = EmbeddingRetriever(
+        document_store=ds,
+        embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1",
+        model_format="sentence_transformers",
+    )
+    ds.update_embeddings(retriever)
+    reader = FARMReader("deepset/minilm-uncased-squad2", use_gpu=False)
+    p = Pipeline()
+    p.add_node(component=retriever, name='retriever', inputs=['Query'])
+    p.add_node(component=reader, name='reader', inputs=['retriever'])
+
+
+def main():
+    app_init()
+    st.title("Haystack Demo")
+    input = st.text_input("Query ...")
+    st.text(p.run(input))
+
+if __name__ == "__main__":
+    main()
